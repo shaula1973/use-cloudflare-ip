dc68c4f112e9575a0354423d07d54bb924696950
shaula1973 <86102828+shaula1973@users.noreply.github.com>
2022-02-25 23:04:01 +0800

    Update cf-openwrt-auto.sh

--- cf-openwrt-auto.sh
+++ cf-openwrt-auto.sh
@@ -4,14 +4,14 @@
 cd /panyi/cloudflare
 
 if [[ ! -f "CloudflareST" ]]; then
-	wget -N https://github.com/XIU2/CloudflareSpeedTest/releases/download/v2.0.2/CloudflareST_linux_arm64.tar.gz
-	tar -xvf CloudflareST_linux_arm64.tar.gz
+	wget -N https://github.com/XIU2/CloudflareSpeedTest/releases/download/v2.0.3/CloudflareST_linux_386.tar.gz
+	tar -xvf CloudflareST_linux_386.tar.gz
 	chmod +x CloudflareST
 fi
 
 ##注意修改！！！
-/etc/init.d/haproxy stop
-/etc/init.d/passwall stop
+#/etc/init.d/haproxy stop
+#/etc/init.d/passwall stop
 wait
 
 ./CloudflareST -dn 10 -tll 40 -o cf_result.txt
@@ -26,10 +26,8 @@ if [[ -f "cf_result.txt" ]]; then
 	uci commit passwall
 	wait
 	##注意修改！！！
-	sed -i "s/$(uci get passwall.xxxxxxxxxx.address)/${first}/g" /etc/config/passwall
-	sed -i "s/$(uci get passwall.xxxxxxxxxx.address)/${second}/g" /etc/config/passwall
-	sed -i "s/$(uci get passwall.xxxxxxxxxx.address)/${third}/g" /etc/config/passwall
-	wait
+	sed -i "s/$(uci get passwall.03091b605d5e410aa300b8c6379afc9d.address)/${first}/g" /etc/config/passwall
+	it
 	uci commit passwall
 	wait
 	[[ $(/etc/init.d/haproxy status) != "running" ]] && /etc/init.d/haproxy start
